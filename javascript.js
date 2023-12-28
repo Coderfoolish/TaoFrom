7dc5391b71c3ca3a3df101beb2a0d9b3d5ec2250
Dino? <daophuduytan2203@gmail.com>
2023-12-28 23:42:49 +0700

    tao form

---
+++ javascript.js
@@ -0,0 +1,72 @@
+let signin_form = document.querySelector('#signin-form')
+
+let signin_btn = document.querySelector('#signin-btn')
+
+validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+}
+checkSigninInput = (input) => {
+    let err_span = signin_form.querySelector(`span[data-err-for="${input.id}"]`)
+    let val = input.value.trim()
+    let form_group = input.parentElement
+
+    switch(input.getAttribute('type')) {
+        case 'password':
+            if (val.length < 6) {
+                form_group.classList.add('err')
+                form_group.classList.remove('success')
+                err_span.innerHTML = 'Password must be at least 6 characters'
+            } else {
+                form_group.classList.add('success')
+                form_group.classList.remove('err')
+                err_span.innerHTML = ''
+            }
+            break;
+        case 'email':
+            if (val.length === 0 || !validateEmail(val)) {
+                form_group.classList.add('err')
+                form_group.classList.remove('success')
+                err_span.innerHTML = 'Email is invalid'
+            } else {
+                form_group.classList.add('success')
+                form_group.classList.remove('err')
+                err_span.innerHTML = ''
+            }
+    }
+}
+
+checkSigninForm = () => {
+    let inputs = signin_form.querySelectorAll('.form-input')
+    inputs.forEach(input => checkSigninInput(input))
+}
+
+signin_btn.onclick = () => {
+    checkSigninForm()
+}
+
+let inputs = signin_form.querySelectorAll('.form-input')
+inputs.forEach(input => {
+    input.addEventListener('focusout', () => {
+        checkSigninInput(input)
+    })
+})
+
+function showSignUp(){
+    document.getElementById('signin-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
+function showLogin(){
+document.getElementById('signup-form').style.display = 'none';
+document.getElementById('signin-form').style.display = 'block';
+}
+
+function createAdmin () {
+    if (localStorage.getItem('user ') == null ) 
+    {
+        var userArray = [] ; 
+        var user = {email: 'admin@gmail.com' , password: 'admin' } ; 
+        userArray.push(user) ; 
+        localStorage.setItem('user' , JSON.stringify(userArray)) ;
+    }
+}
